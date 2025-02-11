af8ba86b3fe093a890b962aafa0023727daa7c3c
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-11 11:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const student = {
   name: "John",
   age: 30,
   city: "New York",
-  
+
   getKeys: function () {
-    return Object.keys(this).filter(key => key !== "getKeys");
+    return Object.keys(this);
   }
 };
 
-console.log(student.getKeys()); 
+console.log(student.getKeys());
